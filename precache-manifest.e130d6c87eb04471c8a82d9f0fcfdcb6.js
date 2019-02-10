f03a532a1af7d476fa66efd6e921c718f034578a
adamsinnott <adamsinnott@gmail.com>
2019-02-10 18:28:15 +0000

    Updates

---
+++ precache-manifest.e130d6c87eb04471c8a82d9f0fcfdcb6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "26ead58317a649cdf406",
+    "url": "/tic-tac-toe/static/js/runtime~main.26ead583.js"
+  },
+  {
+    "revision": "0335d2b54ebcdcb3b11b",
+    "url": "/tic-tac-toe/static/js/main.0335d2b5.chunk.js"
+  },
+  {
+    "revision": "62ea0cedbc773255257f",
+    "url": "/tic-tac-toe/static/js/1.62ea0ced.chunk.js"
+  },
+  {
+    "revision": "0335d2b54ebcdcb3b11b",
+    "url": "/tic-tac-toe/static/css/main.e55f4698.chunk.css"
+  },
+  {
+    "revision": "50368488e3696db571e9ee04e9d94003",
+    "url": "/tic-tac-toe/index.html"
+  }
+];
